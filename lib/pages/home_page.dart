3b6369b40f7aee591c35dadf672f4e845df87793
helloabhi24 <ankitanuj078@gmail.com>
2021-04-29 17:14:00 +0530

    Grid Added(day 16)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,11 +43,37 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(color: Colors.deepPurple),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(color: Colors.black),
+                        child: Text(item.price.toString(),
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  );
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items[index],
-                ),
               )
             : Center(child: CircularProgressIndicator()),
       ),
