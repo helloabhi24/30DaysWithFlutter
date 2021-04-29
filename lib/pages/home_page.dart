f83166d93c7f223b6e5e6baf82954d6322053187
helloabhi24 <ankitanuj078@gmail.com>
2021-04-29 12:41:40 +0530

    local file load (day 13)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = 'CodePur';
+
   final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeJson = jsonDecode(catalogJson);
+    var productsData = decodeJson["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
